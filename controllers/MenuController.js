e6d4f359f00b22c01a984c285e32d97ab79adeae
Zart Seri <zartseri@yahoo.com>
2018-12-31 16:21:58 -0500

    Checkpoint 4 listing: completed

--- controllers/MenuController.js
+++ controllers/MenuController.js
@@ -9,7 +9,9 @@ const ContactController = require("./ContactController");
         message: "Please choose from an option below: ",
         choices: [
           "Add new contact",
-          "Get Date",
+          "View all contacts",
+          "Search for a contact",
+          "Get today's date",
           "Exit"
         ]
       }
@@ -27,6 +29,12 @@ const ContactController = require("./ContactController");
            case "Get Date":
            this.getDate();
            break;
+           case "View all contacts":
+           this.getContacts();
+           break;
+           case "Search for a contact":
+                 this.search();
+                 break;
            case "Exit":
            this.exit();
            default:
@@ -38,6 +46,76 @@ const ContactController = require("./ContactController");
        console.log(err);
      });
    }
+
+   delete(contact){
+         inquirer.prompt(this.book.deleteConfirmQuestions)
+         .then((answer) => {
+           if(answer.confirmation){
+             this.book.delete(contact.id);
+             console.log("contact deleted!");
+             this.main();
+           } else {
+             console.log("contact not deleted");
+             this.showContact(contact);
+           }
+         })
+         .catch((err) => {
+           console.log(err);
+           this.main();
+         });
+       }
+
+   search(){
+         inquirer.prompt(this.book.searchQuestions)
+         .then((target) => {
+          this.book.search(target.name)
+          .then((contact) => {
+             if(contact === null){
+               this.clear();
+               console.log("contact not found");
+               this.search();
+             } else {
+               this.showContact(contact);
+            }
+
+           });
+        })
+        .catch((err) => {
+          console.log(err);
+          this.main();
+        });
+       }
+
+       showContact(contact){
+         this._printContact(contact);
+         inquirer.prompt(this.book.showContactQuestions)
+    .then((answer) => {
+      switch(answer.selected){
+        case "Delete contact":
+          this.delete(contact);
+          break;
+        case "Main menu":
+          this.main();
+          break;
+        default:
+          console.log("Something went wrong.");
+          this.showContact(contact);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      this.showContact(contact);
+    });
+       }
+
+       _printContact(contact){
+         console.log(`
+           name: ${contact.name}
+           phone number: ${contact.phone}
+           email: ${contact.email}
+           ---------------`
+         );
+       }
 
    getDate(){
      this.clear();
@@ -59,13 +137,33 @@ const ContactController = require("./ContactController");
      this.main();
    }
 
+   getContacts(){
+      this.clear();
+
+      this.book.getContacts().then((contacts) => {
+        for (let contact of contacts) {
+          console.log(`
+          name: ${contact.name}
+          phone number: ${contact.phone}
+          email: ${contact.email}
+          ---------------`
+          );
+        }
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    }
+
+
    clear(){
      console.log("\x1Bc");
    }
    addContact(){
         this.clear();
         inquirer.prompt(this.book.addContactQuestions).then((answers) => {
-          this.book.addContact(answers.name, answers.phone).then((contact) => {
+          this.book.addContact(answers.name, answers.phone, answers.email).then((contact) => {
             console.log("Contact added successfully!");
             this.main();
           }).catch((err) => {
